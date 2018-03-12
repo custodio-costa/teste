1bab7b1115bf65e4f4427da57e82a9c576de0f52
custodio <custodio@halley>
2018-03-12 09:50:39 -0300

    # WARNING: head commit changed in the meantime
    Start and Stop server junit

--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -1,25 +1,51 @@
 package br.com.alura.loja;
 
+import java.net.URI;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
 import junit.framework.Assert;
 
 public class ClienteTest {
+
+	
+   private HttpServer server;
+
+@Before
+   public void startaServidor() {
+		server = Servidor.inicializaServidor();
+   }
+   
+   @After
+   public void mataServidor() {
+	   server.stop();
+   }
 	
 	@Test
-	public void testaQueAConexaoComOServidorfunciona() {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://www.mocky.io");
-		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
-//		System.out.println(conteudo);
-		Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));	
-	}
+    public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+        String conteudo = target.path("/carrinhos").request().get(String.class);
+        System.out.println(conteudo);
+        Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+        Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+    }
+
 	
-	@Test
+/*
+ * 	@Test
     public void testaQueAConexaoComOServidorFuncionaNoPathDeProjetos() {
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target("http://localhost:8080");
@@ -27,6 +53,20 @@ public class ClienteTest {
         System.out.println(conteudo);
         Assert.assertTrue(conteudo.contains("<nome>Minha loja"));
     }
+ * 
+ */
 
+/*
+ * 	@Test
+	public void testaQueAConexaoComOServidorfunciona() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://www.mocky.io");
+		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
+		System.out.println(conteudo);
+		Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));	
+	}
+		
+ */
+	
 
 }
